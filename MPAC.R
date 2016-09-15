2e20fcd147e0d7aef471bac45aa5b780750ffb62
Kristin Doucet <kristin.doucet@mpac.ca>
2016-09-15 10:17:26 -0400

    Reverted the terrible changes from the last commit

--- MPAC.R
+++ MPAC.R
@@ -1,12 +1,12 @@
-#Read in theaaaaaaaaaaa (fake) 2020.csv file
+#Read in the (fake) 2020.csv file
 # Read in a text file and make sure that the first column is character
-# http://stackoverflow.com/quesuaoia[f'poiepoajlations/2805357/specifying-colclasses-in-the-read-csv
-column_classes=c("character","numeric","characteraiurfakjel;kja")
-data<-read.csv("validsssssssssssssssssssate-vars.txt", colClasses=column_classes)
+# http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
+column_classes=c("character","numeric","character")
+data<-read.csv("validate-vars.txt", colClasses=column_classes)
 
 
-#frequeancy counts
-#compaaaaaaaare means
-#croaaaaaaaass tabs
-#validataaaaaaaae variables
-#summinaaaaaaaaaaag by ID
+#frequency counts
+#compare means
+#cross tabs
+#validate variables
+#summing by ID
